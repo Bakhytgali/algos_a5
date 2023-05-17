eea6f07a5068c69ab7bfbbed45fef42de2eff595
Your Name <omgwtfwat@gmail.com>
2023-05-17 08:14:02 +0600

    Creating BST class and inner-class "Node"

---
+++ src/BST.java
@@ -0,0 +1,11 @@
+public class BST<K extends Comparable<K>, V> {
+    private class Node {
+        private K key;
+        private V value;
+        private Node left, right;
+        public Node(K key, V value){
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
