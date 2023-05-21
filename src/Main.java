69d8772ef0cb58e2c6ab221cc5e0c3b3166e4a9c
Your Name <omgwtfwat@gmail.com>
2023-05-21 21:50:47 +0600

    Testings for getSize(), get() and delete() functions

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@ public class Main {
     public static void main(String[] args) {
         BST<Integer, String> searchTree = new BST<>();
         searchTree.put(5, "Luffy");
+        searchTree.put(1, "Shanks");
+        searchTree.put(3, "Mihawk");
+        System.out.println(searchTree.getSize());
+        System.out.println(searchTree.get(5));
+        searchTree.delete(3);
+        System.out.println(searchTree.get(3));
     }
 }
