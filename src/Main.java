1d4d970cdabd92b5225214a0132055755a90dd59
Your Name <omgwtfwat@gmail.com>
2023-05-21 21:48:22 +0600

    testing for put function

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BST<Integer, String> searchTree = new BST<>();
+        searchTree.put(5, "Luffy");
     }
 }
